4c798acac6d1a8d6e20dde59ef16d772151eafd7
habiba <hkhallaf5@gmail.com>
2026-02-06 23:50:38 +0200

    Update rockpaperscissors.js

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,37 +1,39 @@
- let score=JSON.parse(localStorage.getItem('score'))||
+  let score=JSON.parse(localStorage.getItem('score'))||
       {
         Wins:0,
         Losses:0,
         Ties:0
       };
       updatedscore();
-      document.querySelector('.rock-move-button').addEventListener('click',()=>{playerplaymode('Rock');})
-      document.querySelector('.paper-move-button').addEventListener('click',()=>{playerplaymode('Paper');})
-      document.querySelector('.scissors-move-button').addEventListener('click',()=>{playerplaymode('Scissors');})
-      document.querySelector('.reset-button').addEventListener('click',()=>{ reset();
-        score.Wins=0;
-        score.Losses=0;
-        score.Ties=0;
-        updatedscore();
-        localStorage.removeItem('score');})
-      document.querySelector('.autoplay-button').addEventListener('click',()=>{interval();
-              change();});
+      document.querySelector('.rock-move-button').addEventListener('click',()=>{playerplaymode('rock');})
+      document.querySelector('.paper-move-button').addEventListener('click',()=>{playerplaymode('paper');})
+      document.querySelector('.scissors-move-button').addEventListener('click',()=>{playerplaymode('scissors');})
+      document.querySelector('.reset-button').addEventListener('click',()=>{ confirmreset();
+       })
+      document.querySelector('.autoplay-button').addEventListener('click',()=>{autoplay();});
       document.body.addEventListener('keydown',(event)=>{
         if(event.key==='r')
-          playerplaymode('Rock');
+          playerplaymode('rock');
         else if(event.key==='p')
-          playerplaymode('Paper');
+          playerplaymode('paper');
         else if(event.key==='s')
         {
-          playerplaymode('Scissors');
+          playerplaymode('scissors');
         }
-
+        else if(event.key==='a')
+        {
+          autoplay();
+        }
+        else if(event.key==='Backspace')
+        {
+          confirmreset();
+        }
       })
       function playerplaymode(playmode)
       {
         const computermode=computerplaymode();
         let result='';
-        if(playmode==='Rock')
+        if(playmode==='rock')
         {
           if(computermode===playmode) 
           {
@@ -46,7 +48,7 @@
             result='You lose!';
           }
         }
-        else if(playmode==='Paper')
+        else if(playmode==='paper')
         {
           if(computermode===playmode) 
           {
@@ -119,34 +121,60 @@
         document.querySelector('.js-score').innerHTML=`
 Wins: ${score.Wins}, Losses: ${score.Losses}, Ties: ${score.Ties}`;
       }
-let autoplay=false;
+let auto=false;
 let intervalid;
-function interval()
+function autoplay()
 { 
   const play=document.querySelector('.autoplay-button');
-  if(autoplay===false)
+  if(auto===false)
   {
   intervalid=setInterval(() =>
   {
     const computemove=computerplaymode();
     playerplaymode(computemove);
   },1000);
-  autoplay=true;
+  auto=true;
    play.innerHTML='Stop Play';
   }
   else
   {
     clearInterval(intervalid);
-    autoplay=false;
+    auto=false;
     play.innerHTML='Auto Play';
   }
-
 }
 function reset()
 {
-  
     document.querySelector('.js-result').innerHTML='';
     document.querySelector('.js-move').innerHTML='';
-  
+    score.Wins=0;
+    score.Losses=0;
+    score.Ties=0;
+    updatedscore();
+    localStorage.removeItem('score');
+    document.querySelector('.js-confirm-reset').innerHTML='';
 }
-
+function confirmreset()
+{
+  document.querySelector('.js-confirm-reset').innerHTML=`<p class="confirm-text">Are you sure you want to reset the score?<button class="confirm">Yes</button>
+  <button class="do-nothing">No</button></p>`
+  document.querySelector('.confirm').addEventListener('click',()=>{
+  reset();
+  hidemessage();})
+  document.body.addEventListener('keydown',(event)=>{
+    if(event.key==='y')
+    {
+      reset();
+      hidemessage();
+    }
+  })
+  document.querySelector('.do-nothing').addEventListener('click',()=>{hidemessage();})
+  document.body.addEventListener('keydown',(event)=>{
+    if(event.key==='n')
+      hidemessage();
+    })
+}
+function hidemessage()
+{
+  document.querySelector('.js-confirm-reset').innerHTML='';
+}
